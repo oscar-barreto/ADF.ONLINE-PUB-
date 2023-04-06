db543c92cd6f4318613078329a2195e809d425e5
oscar-barreto <88983667+oscar-barreto@users.noreply.github.com>
2023-04-06 00:56:24 -0300

    commit 2

--- dom.js
+++ dom.js
@@ -9,6 +9,9 @@ const mageBtn = document.getElementById("mage-btn");
 const heroClass = document.getElementById("hero-class");
 const warriorBtn = document.getElementById("warrior-btn")
 const btnClassCon = document.getElementById("btn-con");
+const summaryText = document.getElementById("level");
+const capy = document.getElementById("capy");
+const smirnoff = document.getElementById("smirnoff");
 
 
 
@@ -44,23 +47,58 @@ warriorBtn.addEventListener("click", () =>{
     btnClassCon.style.display = 'none'
     heroClass.innerText = "Warrior";
     document.getElementById("class-getter").style.display='none';
-    document.querySelector(".level-summary").innerText = `You have nade your first hard choice by choosing your class which is: ${heroClass.innerHTML}, now you must choose which path you will take. Each path will affect what kind of adversities you may face, some paths will prove to be extremely challenging`;
+    summaryText.innerText = `You have nade your first hard choice by choosing your class which is: ${heroClass.innerHTML}, now you must choose which path you will take. Each path will affect what kind of adversities you may face, some paths will prove to be extremely challenging`;
     const proceedBtn =document.createElement("button");
     proceedBtn.innerText = 'Proceed';
-    document.querySelector(".proceed-container").appendChild(proceedBtn)
+    proceedBtn.classList.add("pro-btn");
+    document.querySelector(".proceed-container").appendChild(proceedBtn);
+    document.querySelector(".pro-btn").addEventListener("click", () =>{
+        summaryText.innerText="You were chosen to rescue princess Valentine from the castle of Ilyria, the only way to rescue her is by crossing the entire Kingdom of Abundriel. Fortunately there are multiple ways on how to get to her? Would you like to choose start by the field of Capybaras or the Mines or Smirnoff? "
+        document.querySelector(".pro-btn").style.display = "none";
+        capy.style.display = "block";
+        smirnoff.style.display = "block";
+    })
 });
 mageBtn.addEventListener("click", () =>{
     btnClassCon.style.display = 'none'
     heroClass.innerText = "Mage"
     document.getElementById("class-getter").style.display='none';
-    document.querySelector(".level-summary").innerText = `You have nade your first hard choice by choosing your class which is: ${heroClass.innerHTML}, now you must choose which path you will take. Each path will affect what kind of adversities you may face, some paths will prove to be extremely challenging`
-    document.querySelector(".proceed-container").appendChild(proceedBtn)
-
+    summaryText.innerText =  `You have nade your first hard choice by choosing your class which is: ${heroClass.innerHTML}, now you must choose which path you will take. Each path will affect what kind of adversities you may face, some paths will prove to be extremely challenging`
+    const proceedBtn =document.createElement("button");
+    proceedBtn.innerText = 'Proceed';
+    proceedBtn.classList.add("pro-btn");
+    document.querySelector(".proceed-container").appendChild(proceedBtn);
+    document.querySelector(".pro-btn").addEventListener("click", () =>{
+        summaryText.innerText="You were chosen to rescue princess Valentine from the castle of Ilyria, the only way to rescue her is by crossing the entire Kingdom of Abundriel. Fortunately there are multiple ways on how to get to her? Would you like to choose start by the field of Capybaras or the Mines or Smirnoff? "
+        document.querySelector(".pro-btn").style.display = "none";
+        capy.style.display = "block";
+        smirnoff.style.display = "block";
+    })
 });
 rogueBtn.addEventListener("click", () =>{
     btnClassCon.style.display = 'none'
     heroClass.innerText = "Rogue";
     document.getElementById("class-getter").style.display='none';
-    document.querySelector(".level-summary").innerText = `You have nade your first hard choice by choosing your class which is: ${heroClass.innerHTML}, now you must choose which path you will take. Each path will affect what kind of adversities you may face, some paths will prove to be extremely challenging`
-    document.querySelector(".proceed-container").appendChild(proceedBtn)
+    summaryText.innerText =  `You have nade your first hard choice by choosing your class which is: ${heroClass.innerHTML}, now you must choose which path you will take. Each path will affect what kind of adversities you may face, some paths will prove to be extremely challenging`
+    const proceedBtn =document.createElement("button");
+    proceedBtn.innerText = 'Proceed';
+    proceedBtn.classList.add("pro-btn");
+    document.querySelector(".proceed-container").appendChild(proceedBtn);
+    document.querySelector(".pro-btn").addEventListener("click", () =>{
+        summaryText.innerText="You were chosen to rescue princess Valentine from the castle of Ilyria, the only way to rescue her is by crossing the entire Kingdom of Abundriel. Fortunately there are multiple ways on how to get to her? Would you like to choose start by the field of Capybaras or the Mines or Smirnoff? "
+        document.querySelector(".pro-btn").style.display = "none";
+        capy.style.display = "block";
+        smirnoff.style.display = "block"; 
+    })
 });
+
+capy.addEventListener("click", ()=>{
+    summaryText.innerText = "You arrive at the field of Capybaras. But the on the horizon you see a really obese capybara heading to attack you. Get ready to defend yourself!";
+    capy.style.display = "none";
+    smirnoff.style.display = "none"; 
+});
+smirnoff.addEventListener("click", ()=>{
+    summaryText.innerText = "You arrive at the Mines of Smirnoff. But then you see on the horizon an angry dwarf holding his sword charging towards you. Get ready to defend yourself!";
+    capy.style.display = "none";
+    smirnoff.style.display = "none"; 
+})
